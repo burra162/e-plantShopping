6a57656f88f217459428f8e326aa7c519921971d
Burra162 <s.burra@eagles.oc.edu>
2025-05-22 20:36:02 -0400

    delete button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -289,15 +289,32 @@ function ProductList({ onHomeClick }) {
                 </div>
                 <div style={styleObjUl}>
                     <div><a href="#" onClick={handlePlantsClick} style={styleA}>Plants</a></div>
-                    <div><a href="#" onClick={handleCartClick} style={styleA}>
-                        <h1 className='cart'>
-                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
-                                <circle cx="80" cy="216" r="12"></circle>
-                                <circle cx="184" cy="216" r="12"></circle>
-                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
-                            </svg>
-                        </h1>
-                    </a></div>
+                    <div style={{ position: 'relative' }}>
+    <a href="#" onClick={handleCartClick} style={styleA}>
+        <h1 className='cart'>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" height="68" width="68">
+                <circle cx="80" cy="216" r="12"></circle>
+                <circle cx="184" cy="216" r="12"></circle>
+                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"></path>
+            </svg>
+        </h1>
+        {cartItems.length > 0 && (
+            <span style={{
+                position: 'absolute',
+                top: '-5px',
+                right: '-5px',
+                background: 'red',
+                color: 'white',
+                borderRadius: '50%',
+                padding: '4px 8px',
+                fontSize: '14px'
+            }}>
+                {cartItems.length}
+            </span>
+        )}
+    </a>
+</div>
+
                 </div>
             </div>
 
